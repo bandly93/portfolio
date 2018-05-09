c54cdd82c349a07cf7c75feb4982069092ea26b4
Band Ly <bandly93@gmail.com>
2018-05-09 11:41:34 -0700

    add automatic slideshow

---
+++ src/shared/data/homeOptions.js
@@ -0,0 +1,19 @@
+export const homeOptions = [
+	{
+		text : 'is server-side rendered, which provides web-crawlers with content for search engine functionalities.',
+		images : ['','','','',''],
+	},
+	{
+		text : 'was developed using modern frameworks such as React, Redux, Express, and Node.',
+		images : ['','','',''],
+	},
+
+	{
+		text : 'is using brotli compression algorithm to reduce the amount of data downloaded for initial page load. This improved the speed of the webpage and ultimately reduced data cost by 100%.',
+		images : ['','',''],
+	},
+	{
+		text : 'was designed to be responsive on all screen sizes. Try resizing the browser or viewing from a different size device.', 
+		images : ['','','',''],
+	},
+]
--- src/shared/views/Home.js
+++ src/shared/views/Home.js
@@ -1,10 +1,49 @@
 import React, { Fragment,Component } from 'react';
+import { homeOptions } from '../data/homeOptions';
 
 class Home extends Component{
+	
+		
+	banner = () => {
+		return <div className ='home-div'>
+			<h1> Little About Me </h1>
+			<p> Self-taught freelance web developer. Majored in Business Management but passionate in problem solving, which then lead me to pursue computer science.</p>
+		</div>
+	}
+
+	siteInformationSlide = () => {	
+		return homeOptions.map(({},i) => (
+			<p key = {i} className = 'slideDiv'>{homeOptions[i].text}</p>	
+			)
+		)
+	}
+
+	carousel = (index) => {
+		let i;
+		let x = document.getElementsByClassName('slideDiv');
+		for ( i = 0 ; i < x.length ; i++){
+			x[i].style.display = 'none';
+		}
+		index++
+		if(index > x.length) {
+			index = 1;
+		}
+		
+		x[index-1].style.display = 'block';
+		setTimeout(()=>this.carousel(index),4000);
+		
+	}
+
 	render(){
-		return<Fragment>
-			<p> Welcome to Band Ly's Portfolio page. </p>
-		</Fragment>
+		let index = 0;
+		let slides = document.getElementsByClassName('slideDiv');
+		slides.length > 1 ? this.carousel(index):null	
+		
+		return<div className = 'home-div'>
+			{this.banner()}
+			<h1> My Portfolio ... </h1>
+			{this.siteInformationSlide()}
+		</div>
 	}
 }
 
