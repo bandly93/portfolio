4d212e27ef18513916c77feb4c5da22695248fc6
Band Ly <bandly93@gmail.com>
2018-05-24 09:34:03 +0000

    update html

--- src/server/render.js
+++ src/server/render.js
@@ -28,12 +28,16 @@ export const renderFullPage = (html,preloadedState) => {
         <title>Portfolio</title>
 				<meta name = 'viewport' content = 'width=device-width'>
 				<link rel="icon" href="data:;base64,iVBORwOKGO=">
-				<link href = '/client-bundle.css' rel='stylesheet'>
+				<style>
+					div{list-style-type:none;-webkit-padding-start:0}body,html{color:black;font-family:Arial,Helvetica,sans-serif;background-image:url('./images/background.jpg');margin:0;padding:0}a{color:black;text-decoration:none;padding:0 0.5vw}#hamburger-icon{content:url('./images/hamburger.svg');height:40px;margin-right:2vw}.navbar{background-color:white;display:inline-flex;text-align:center;width:100%}.navbar div{margin:auto 0}#logo{content:url('./images/logo.svg');background-repeat:no-repeat;width:80px}@media only screen and (min-width:813px){#navbar-secondary{display:none}#navbar-primary{justify-content:space-around}}@media only screen and (max-width:812px){#navbar-primary{display:none}#navbar-secondary{justify-content:space-between}.on{display:inline-grid}.off{display:none}}										
+				</style>
 			</head>
       <body>
         <div id="root">${html}</div>
-				<script src= '/client-bundle.js' async = true></script>
+				<script src= '/client-bundle.js' async></script>
 				<script> window.__PRELOADED_STORE__ = ${serialize(preloadedState)} </script>
+				
+				<link href = '/client-bundle.css' rel='stylesheet'>
       </body>
     </html>
   `
