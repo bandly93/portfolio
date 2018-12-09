6158e47b1350699cbd4dc25c32e7c6e3c57b1299
Band Ly <bandly93@gmail.com>
2018-12-09 13:20:41 -0800

    update

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,27 +4,9 @@ import { updateView } from '../shared/redux/viewModule.js';
 import styles from './styles.css';
 import NavBar from './NavBar.js';
 import { withRouter } from 'react-router-dom';
-import ga from 'react-ga';
-import {GOOGLE_ANALYTICS} from '../../config.json';
 
 class App extends Component{
 		
-	componentDidMount(){
-		this.view();
-
-		window.addEventListener('resize',()=>this.view());
-
-		ga.initialize(GOOGLE_ANALYTICS,{debug:false});
-		console.log(this.props.location.pathname);
-		ga.pageview(this.props.location.pathname);
-	}
-	
-	componentWillUpdate(nextProps){
-		if(nextProps.location.pathname !== this.props.location.pathname){
-			ga.pageview(nextProps.location.pathname);
-		}
-	}
-	
 	view = () => {
 		const { innerHeight,innerWidth } = window;
 		const { updateView } = this.props;
@@ -32,7 +14,6 @@ class App extends Component{
 	}	
 
 	render(){
-		console.log(this.props.location.pathname);
 		return<div className ='profile'>
 				<NavBar />	
 		</div>
